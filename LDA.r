da1b6aa43015eefe24b78d7df48cf22dbfe334b1
alessiobar <47196616+alessiobar@users.noreply.github.com>
2022-11-26 20:52:06 +0100

    Rename thesisR.r to LDA.r

--- LDA.r
+++ LDA.r
@@ -18,7 +18,7 @@ dtm <- DocumentTermMatrix(docs1)
 lda <- LDA(dtm, k = 10, method = "Gibbs", 
             control = list(seed = 42, iter = 5000, verbose = 50, alpha = 0.0)) 
 
-#cambiando alpha mi cambia il modello, co 5 veniva 0.103 il primo pvalue su ARE, co 2.5 0.004, co 1.0 è 0.002, co 0 è 0 :DDDD
+#cambiando alpha mi cambia il modello, co 5 veniva 0.103 il primo pvalue su ARE, co 2.5 0.004, co 1.0 Ã¨ 0.002, co 0 Ã¨ 0 :DDDD
 attr(lda, "alpha")
 
 tmResult <- posterior(lda)
