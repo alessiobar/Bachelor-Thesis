2ffdb8db726e52918c57f7b449fe67c042ac55a7
alessiobar <47196616+alessiobar@users.noreply.github.com>
2022-11-26 20:55:31 +0100

    Add files via upload

---
+++ YoutubeToMp3.py
@@ -0,0 +1,30 @@
+import youtube_dl
+from youtubesearchpython import VideosSearch
+import os
+os.chdir(r"C:/Users/alema/Desktop")
+import pandas as pd
+df = pd.read_excel('outttt.xlsx')
+os.chdir("Thesis/Tracks")
+
+links=[]
+ll=[]
+for x in range(len(df["Song_ID"])):
+    ll.append(df["Song"][x]+" - "+df["Artist"][x])
+ll=list(set(ll)) #to remove double entries
+
+for x in ll:
+    videosSearch = VideosSearch(x, limit = 1)
+    try:
+        lk=videosSearch.result()["result"][0]["link"]
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': "{} - {}.{}".format(x[:x.find("-")-1], x[x.find("-")+2:],"mp3"),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }]}
+        youtube_dl.YoutubeDL(ydl_opts).download([lk])
+
+    except Exception:
+        print(x)
