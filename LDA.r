2cb5e20f91e9b3e616f004381f8e086e63319690
alessiobar <47196616+alessiobar@users.noreply.github.com>
2022-11-28 01:30:56 +0100

    Update LDA.r

--- LDA.r
+++ LDA.r
@@ -1,30 +1,29 @@
+"""
+This script runs LDA using Gibbs sampling at 5,000 iterations, as specified in the supplementary material of Berger et al.
+"""
 library(readxl)
-df <- read_excel("C:/Users/alema/Desktop/perR2.xlsx")
-head(df)
-
-#install.packages("Rcpp")
 library(topicmodels)
 library(tm)
 library(Rcpp)
 library(writexl)
 
-df<-as.data.frame(df)
+df <- read_excel("PyToR.xlsx")
+head(df)
+df <- as.data.frame(df)
 df[df == 0] <- "Void"
 df[is.na(df)] <- "Void"
-#df<-subset(df, Lyrics!="Void")
+df <- subset(df, Lyrics!="Void")
 
-docs1 <- Corpus(VectorSource(df$Lyrics) )
-dtm <- DocumentTermMatrix(docs1)
+docs1 <- Corpus(VectorSource(df$Lyrics))
+dtm <- DocumentTermMatrix(docs1, control = list(weighting=weightTf, stopwords = TRUE ))
 lda <- LDA(dtm, k = 10, method = "Gibbs", 
-            control = list(seed = 42, iter = 5000, verbose = 50, alpha = 0.0)) 
+            control = list(seed = 42, iter = 5000, verbose = 100, alpha = 5)) 
 
-#cambiando alpha mi cambia il modello, co 5 veniva 0.103 il primo pvalue su ARE, co 2.5 0.004, co 1.0 è 0.002, co 0 è 0 :DDDD
 attr(lda, "alpha")
-
 tmResult <- posterior(lda)
 attributes(tmResult)
 terms(lda, 10)
 
 theta <- tmResult$topics
 theta <- as.data.frame(theta)
-write_xlsx(theta,"C:/Users/alema/Desktop/perPydaR2.xlsx")
+write_xlsx(theta,"perPydaR.xlsx")
